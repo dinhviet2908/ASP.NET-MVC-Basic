1dc3cd609cc19a525074e137d4feac3cfd7aad4a
dinhviet2908 <phamdinhviet290899@gmail.com>
2021-06-03 23:21:03 +0700

    Action selector and Action verbs

---
+++ MVCCoreApp/MVCCoreApp/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCCoreApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCCoreApp.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //Fail: http://localhost:port/product/edit
+        //Ok: http://localhost:port/product/modify
+        //[ActionName("Modify")] // ~~ [Route("Product/Modify")]
+        //[NonAction]
+
+        [HttpGet]
+        public string Edit(string id)
+        {
+            return "From edit method";
+        }
+
+        [HttpPost]
+        public IActionResult Edit(ProductModel model)
+        {
+            return RedirectToAction("Index","Product");
+        }
+    }
+}
