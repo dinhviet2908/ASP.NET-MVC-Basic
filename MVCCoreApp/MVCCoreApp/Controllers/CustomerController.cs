8ffc9b01f33202bf60f2b8217a7a0690936115ae
dinhviet2908 <phamdinhviet290899@gmail.com>
2021-06-06 15:50:45 +0700

    Binding data from Controller to View

--- MVCCoreApp/MVCCoreApp/Controllers/CustomerController.cs
+++ MVCCoreApp/MVCCoreApp/Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVCCoreApp.Models.Customer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,17 +9,24 @@ namespace MVCCoreApp.Controllers
 {
     public class CustomerController : Controller
     {
-        [Route("Customer/Customer1")]
-        public string Customer1()
-        {
-            return "Customer1";
-        }
+        //[Route("Customer/Customer1")]
+        //public string Customer1()
+        //{
+        //    return "Customer1";
+        //}
 
-        //[Route("")]
-        [Route("Customer/Customer2")]
-        public string Customer2()
+        ////[Route("")]
+        //[Route("Customer/Customer2")]
+        //public string Customer2()
+        //{
+        //    return "Customer2";
+        //}
+
+        public IActionResult Index()
         {
-            return "Customer2";
+            //ViewBag.Customer = new Customer();
+            var customer = new Customer();
+            return View(customer);
         }
     }
 }
---
+++ MVCCoreApp/MVCCoreApp/Models/Customer/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCCoreApp.Models.Customer
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public Customer()
+        {
+            CustomerId = 1;
+            Name = "Steven Pham";
+            Address = "Viet Nam";
+        }
+    }
+}
