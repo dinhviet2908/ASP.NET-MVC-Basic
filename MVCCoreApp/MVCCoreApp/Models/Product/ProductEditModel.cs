d51b10df9be39049be80f746ce2a4880cabd08e1
dinhviet2908 <phamdinhviet290899@gmail.com>
2021-06-07 21:22:29 +0700

    Tag helpers (continue)

--- MVCCoreApp/MVCCoreApp/Models/Product/ProductEditModel.cs
+++ MVCCoreApp/MVCCoreApp/Models/Product/ProductEditModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,12 @@ namespace MVCCoreApp.Models.Product
     public class ProductEditModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Error message was changed!")]
         public string Name { get; set; }
         public decimal Rate { get; set; }
         public int Rating { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        public DateTime CreateDate { get; set; }
     }
 }
